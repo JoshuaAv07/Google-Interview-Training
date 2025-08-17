f199eb0af33b961c586d792f96464d16fa89bdfa
Joshua Aviles <a6520150001@utch.edu.mx>
2025-08-16 21:33:39 -0600

    add: weighted graphs

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -18,7 +18,6 @@ class UndirectedGraph(Graph):
         # Makes sure the vertex exists
         self.add_vertex(u)
         self.add_vertex(v)
-        
         # Defines u -> v and v -> u
         self.adj_list[u].append(v)
         self.adj_list[v].append(u) 
@@ -28,9 +27,46 @@ class DirectedGraph(Graph):
         # Makes sure the vertex exists
         self.add_vertex(u)
         self.add_vertex(v)
-        
         # Defines u -> v 
         self.adj_list[u].append(v)
         
+import heapq        
+
+class WeightedGraph(DirectedGraph):
+    def add_edge(self, u, v, weight = 1):
+        self.add_vertex(u)
+        self.add_vertex(v)
+        # Defines u -> v 
+        self.adj_list[u].append((v , weight))
         
+    def dijkstra(self, start):
+        # Creates a new dictionary whith the values of each of them to infinite/float('inf')
+        distances = { v: float('inf') for v in self.adj_list }
+        # Set start (the place where you want to start) as 0
+        distances[start] = 0
+        # Creates a tuple of the values of 0 and the vertex name
+        heap = [(0, start)] # -> (0, "A")
+        
+        while heap:
+            # Gets the values from the tuple with the smallest distance
+            current_dist, u = heapq.heappop(heap)
+            
+            # Skip this vertex because  we found a shorter path
+            if current_dist > distances[u]:
+                continue
+             
+            # For each vertex and weight
+            for v, weight in self.adj_list[u]:
+                # Distance to neighbor (v) through the smallest weight (u)
+                distance = current_dist + weight
+                
+                # If this path is shorter than any previous one 
+                if distance < distances[v]:
+                    # Update the shortest
+                    distances[v] = distance
+                    # Insert the new value
+                    heapq.heappush(heap, (distance, v))
+                    
+        return distances
+    
         
--- data_structures/graph_test.py
+++ data_structures/graph_test.py
@@ -1,4 +1,4 @@
-from graph import UndirectedGraph, DirectedGraph
+from graph import UndirectedGraph, DirectedGraph, WeightedGraph
 
 ug = UndirectedGraph()
 print("Undirected Graph: ")
@@ -15,3 +15,17 @@ dg.add_edge("A", "C")
 dg.add_edge("B", "D")
 dg.add_edge("C", "D")
 dg.display()
+
+wg = WeightedGraph()
+print("\nWeighted Directed Graph: ")
+wg.add_edge("A", "B", 5)
+wg.add_edge("A", "C", 2)
+wg.add_edge("B", "D", 1)
+wg.add_edge("C", "D", 7)
+wg.add_edge("C", "B", 3)
+wg.display()
+
+print("\nShortest distances from A using Dijkstra:")
+distances = wg.dijkstra("A")
+for vertex, dist in distances.items():
+    print(f"{vertex}: {dist}")
